67e514fada48ec6bc2582cbf31b38c5e615d8b38
Cas-svg <edgarmakwasha0@gmail.com>
2025-02-02 14:55:37 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Malvin',
   botName: "JINWOO Bot",
   botOwner: 'Malvin King', // Your name
-  ownerNumber: '263714757857', //Your number
+  ownerNumber: '263778858246', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
